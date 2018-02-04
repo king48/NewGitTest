08e179506203eb32bf5e4cfaea5a4bfafb0bd7d5
JD <jd@fakeurl.ca>
2018-02-04 12:26:00 -0500

    added the java file

---
+++ NewGitTest.java
@@ -0,0 +1,15 @@
+// The "NewGitTest" class.
+import java.awt.*;
+import hsa.Console;
+
+public class NewGitTest
+{
+    static Console c;           // The output console
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	
+	// Place your program here.  'c' is the output console
+    } // main method
+} // NewGitTest class
