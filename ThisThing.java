4dfc669283a199d70995bafb0088d2dcae33d140
JD <jd@fakeurl.ca>
2018-02-04 13:30:21 -0500

    This is a new File

---
+++ ThisThing.java
@@ -0,0 +1,15 @@
+// The "ThisThing" class.
+import java.awt.*;
+import hsa.Console;
+
+public class ThisThing
+{
+    static Console c;           // The output console
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	//This part is new and fun!
+	// Place your program here.  'c' is the output console
+    } // main method
+} // ThisThing class
